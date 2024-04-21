b7075e4da713d58445d37cc863f6c2a326c42445
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-21 00:11:52 -0500

    Generate a lot of recipes and tags

--- src/main/java/com/teamabnormals/atmospheric/core/data/server/AtmosphericRecipeProvider.java
+++ src/main/java/com/teamabnormals/atmospheric/core/data/server/AtmosphericRecipeProvider.java
@@ -4,7 +4,10 @@ import com.teamabnormals.atmospheric.core.Atmospheric;
 import com.teamabnormals.atmospheric.core.other.AtmosphericBlockFamilies;
 import com.teamabnormals.atmospheric.core.other.tags.AtmosphericItemTags;
 import com.teamabnormals.atmospheric.core.registry.AtmosphericItems;
+import com.teamabnormals.atmospheric.integration.boatload.AtmosphericBoatTypes;
 import com.teamabnormals.blueprint.core.other.tags.BlueprintItemTags;
+import com.teamabnormals.boatload.core.data.server.BoatloadRecipeProvider;
+import com.teamabnormals.woodworks.core.data.server.WoodworksRecipeProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
@@ -95,13 +98,88 @@ public class AtmosphericRecipeProvider extends RecipeProvider {
 
 		ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, GRIMWEB.get()).requires(AtmosphericItemTags.GRIMWOOD_LOGS).requires(Items.COBWEB).unlockedBy("has_grimwood", has(AtmosphericItemTags.GRIMWOOD_LOGS)).save(consumer);
 
+		generateRecipes(consumer, AtmosphericBlockFamilies.ROSEWOOD_PLANKS_FAMILY);
+		planksFromLogs(consumer, ROSEWOOD_PLANKS.get(), AtmosphericItemTags.ROSEWOOD_LOGS, 4);
+		woodFromLogs(consumer, ROSEWOOD.get(), ROSEWOOD_LOG.get());
+		woodFromLogs(consumer, STRIPPED_ROSEWOOD.get(), STRIPPED_ROSEWOOD_LOG.get());
 		hangingSign(consumer, ROSEWOOD_HANGING_SIGNS.getFirst().get(), STRIPPED_ROSEWOOD_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.ROSEWOOD);
+		WoodworksRecipeProvider.baseRecipes(consumer, ROSEWOOD_PLANKS.get(), ROSEWOOD_SLAB.get(), ROSEWOOD_BOARDS.get(), ROSEWOOD_BOOKSHELF.get(), CHISELED_ROSEWOOD_BOOKSHELF.get(), ROSEWOOD_LADDER.get(), ROSEWOOD_BEEHIVE.get(), ROSEWOOD_CHEST.get(), TRAPPED_ROSEWOOD_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.ROSEWOOD_PLANKS_FAMILY, AtmosphericItemTags.ROSEWOOD_LOGS, ROSEWOOD_BOARDS.get(), ROSEWOOD_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, ROSEWOOD_LEAVES.get(), ROSEWOOD_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		conditionalNineBlockStorageRecipes(consumer, APPLE_CRATE, RecipeCategory.FOOD, AtmosphericItems.PASSION_FRUIT.get(), RecipeCategory.BUILDING_BLOCKS, PASSION_FRUIT_CRATE.get());
+		conditionalNineBlockStorageRecipesWithCustomUnpacking(consumer, GOLDEN_APPLE_CRATE, RecipeCategory.FOOD, AtmosphericItems.SHIMMERING_PASSION_FRUIT.get(), RecipeCategory.BUILDING_BLOCKS, SHIMMERING_PASSION_FRUIT_CRATE.get(), "shimmering_passion_fruit_from_shimmering_passion_fruit_crate", "shimmering_passion_fruit");
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.MORADO_PLANKS_FAMILY);
+		planksFromLogs(consumer, MORADO_PLANKS.get(), AtmosphericItemTags.MORADO_LOGS, 4);
+		woodFromLogs(consumer, MORADO_WOOD.get(), MORADO_LOG.get());
+		woodFromLogs(consumer, STRIPPED_MORADO_WOOD.get(), STRIPPED_MORADO_LOG.get());
 		hangingSign(consumer, MORADO_HANGING_SIGNS.getFirst().get(), STRIPPED_MORADO_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.MORADO);
+		WoodworksRecipeProvider.baseRecipes(consumer, MORADO_PLANKS.get(), MORADO_SLAB.get(), MORADO_BOARDS.get(), MORADO_BOOKSHELF.get(), CHISELED_MORADO_BOOKSHELF.get(), MORADO_LADDER.get(), MORADO_BEEHIVE.get(), MORADO_CHEST.get(), TRAPPED_MORADO_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.MORADO_PLANKS_FAMILY, AtmosphericItemTags.MORADO_LOGS, MORADO_BOARDS.get(), MORADO_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, MORADO_LEAVES.get(), MORADO_LEAF_PILE.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, FLOWERING_MORADO_LEAVES.get(), FLOWERING_MORADO_LEAVES.get(), Atmospheric.MOD_ID);
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.YUCCA_PLANKS_FAMILY);
+		planksFromLogs(consumer, YUCCA_PLANKS.get(), AtmosphericItemTags.YUCCA_LOGS, 4);
+		woodFromLogs(consumer, YUCCA_WOOD.get(), YUCCA_LOG.get());
+		woodFromLogs(consumer, STRIPPED_YUCCA_WOOD.get(), STRIPPED_YUCCA_LOG.get());
 		hangingSign(consumer, YUCCA_HANGING_SIGNS.getFirst().get(), STRIPPED_YUCCA_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.YUCCA);
+		WoodworksRecipeProvider.baseRecipes(consumer, YUCCA_PLANKS.get(), YUCCA_SLAB.get(), YUCCA_BOARDS.get(), YUCCA_BOOKSHELF.get(), CHISELED_YUCCA_BOOKSHELF.get(), YUCCA_LADDER.get(), YUCCA_BEEHIVE.get(), YUCCA_CHEST.get(), TRAPPED_YUCCA_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.YUCCA_PLANKS_FAMILY, AtmosphericItemTags.YUCCA_LOGS, YUCCA_BOARDS.get(), YUCCA_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, YUCCA_LEAVES.get(), YUCCA_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		conditionalNineBlockStorageRecipes(consumer, APPLE_CRATE, RecipeCategory.FOOD, AtmosphericItems.YUCCA_FRUIT.get(), RecipeCategory.BUILDING_BLOCKS, YUCCA_CASK.get());
+		conditionalNineBlockStorageRecipesWithCustomUnpacking(consumer, APPLE_CRATE, RecipeCategory.FOOD, AtmosphericItems.ROASTED_YUCCA_FRUIT.get(), RecipeCategory.BUILDING_BLOCKS, ROASTED_YUCCA_CASK.get(), "roasted_yucca_fruit_from_roasted_yucca_cask", "roasted_yucca_fruit");
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.ASPEN_PLANKS_FAMILY);
+		planksFromLogs(consumer, ASPEN_PLANKS.get(), AtmosphericItemTags.ASPEN_LOGS, 4);
+		woodFromLogs(consumer, ASPEN_WOOD.get(), ASPEN_LOG.get());
+		woodFromLogs(consumer, STRIPPED_ASPEN_WOOD.get(), STRIPPED_ASPEN_LOG.get());
+		woodFromLogs(consumer, WATCHFUL_ASPEN_WOOD.get(), WATCHFUL_ASPEN_LOG.get());
+		woodFromLogs(consumer, CRUSTOSE_WOOD.get(), CRUSTOSE_LOG.get());
 		hangingSign(consumer, ASPEN_HANGING_SIGNS.getFirst().get(), STRIPPED_ASPEN_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.ASPEN);
+		WoodworksRecipeProvider.baseRecipes(consumer, ASPEN_PLANKS.get(), ASPEN_SLAB.get(), ASPEN_BOARDS.get(), ASPEN_BOOKSHELF.get(), CHISELED_ASPEN_BOOKSHELF.get(), ASPEN_LADDER.get(), ASPEN_BEEHIVE.get(), ASPEN_CHEST.get(), TRAPPED_ASPEN_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.ASPEN_PLANKS_FAMILY, AtmosphericItemTags.ASPEN_LOGS, ASPEN_BOARDS.get(), ASPEN_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, ASPEN_LEAVES.get(), ASPEN_LEAF_PILE.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, GREEN_ASPEN_LEAVES.get(), GREEN_ASPEN_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.LAUREL_PLANKS_FAMILY);
+		planksFromLogs(consumer, LAUREL_PLANKS.get(), AtmosphericItemTags.LAUREL_LOGS, 4);
+		woodFromLogs(consumer, LAUREL_WOOD.get(), LAUREL_LOG.get());
+		woodFromLogs(consumer, STRIPPED_LAUREL_WOOD.get(), STRIPPED_LAUREL_LOG.get());
 		hangingSign(consumer, LAUREL_HANGING_SIGNS.getFirst().get(), STRIPPED_LAUREL_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.LAUREL);
+		WoodworksRecipeProvider.baseRecipes(consumer, LAUREL_PLANKS.get(), LAUREL_SLAB.get(), LAUREL_BOARDS.get(), LAUREL_BOOKSHELF.get(), CHISELED_LAUREL_BOOKSHELF.get(), LAUREL_LADDER.get(), LAUREL_BEEHIVE.get(), LAUREL_CHEST.get(), TRAPPED_LAUREL_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.LAUREL_PLANKS_FAMILY, AtmosphericItemTags.LAUREL_LOGS, LAUREL_BOARDS.get(), LAUREL_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, LAUREL_LEAVES.get(), LAUREL_LEAF_PILE.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, DRY_LAUREL_LEAVES.get(), DRY_LAUREL_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.KOUSA_PLANKS_FAMILY);
+		planksFromLogs(consumer, KOUSA_PLANKS.get(), AtmosphericItemTags.KOUSA_LOGS, 4);
+		woodFromLogs(consumer, KOUSA_WOOD.get(), KOUSA_LOG.get());
+		woodFromLogs(consumer, STRIPPED_KOUSA_WOOD.get(), STRIPPED_KOUSA_LOG.get());
 		hangingSign(consumer, KOUSA_HANGING_SIGNS.getFirst().get(), STRIPPED_KOUSA_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.KOUSA);
+		WoodworksRecipeProvider.baseRecipes(consumer, KOUSA_PLANKS.get(), KOUSA_SLAB.get(), KOUSA_BOARDS.get(), KOUSA_BOOKSHELF.get(), CHISELED_KOUSA_BOOKSHELF.get(), KOUSA_LADDER.get(), KOUSA_BEEHIVE.get(), KOUSA_CHEST.get(), TRAPPED_KOUSA_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.KOUSA_PLANKS_FAMILY, AtmosphericItemTags.KOUSA_LOGS, KOUSA_BOARDS.get(), KOUSA_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, KOUSA_LEAVES.get(), KOUSA_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		WoodworksRecipeProvider.leafPileRecipes(consumer, CURRANT_LEAVES.get(), CURRANT_LEAF_PILE.get(), Atmospheric.MOD_ID);
+
+		generateRecipes(consumer, AtmosphericBlockFamilies.GRIMWOOD_PLANKS_FAMILY);
+		planksFromLogs(consumer, GRIMWOOD_PLANKS.get(), AtmosphericItemTags.GRIMWOOD_LOGS, 4);
+		woodFromLogs(consumer, GRIMWOOD.get(), GRIMWOOD_LOG.get());
+		woodFromLogs(consumer, STRIPPED_GRIMWOOD.get(), STRIPPED_GRIMWOOD_LOG.get());
 		hangingSign(consumer, GRIMWOOD_HANGING_SIGNS.getFirst().get(), STRIPPED_GRIMWOOD_LOG.get());
+		BoatloadRecipeProvider.boatRecipes(consumer, AtmosphericBoatTypes.GRIMWOOD);
+		WoodworksRecipeProvider.baseRecipes(consumer, GRIMWOOD_PLANKS.get(), GRIMWOOD_SLAB.get(), GRIMWOOD_BOARDS.get(), GRIMWOOD_BOOKSHELF.get(), CHISELED_GRIMWOOD_BOOKSHELF.get(), GRIMWOOD_LADDER.get(), GRIMWOOD_BEEHIVE.get(), GRIMWOOD_CHEST.get(), TRAPPED_GRIMWOOD_CHEST.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.sawmillRecipes(consumer, AtmosphericBlockFamilies.GRIMWOOD_PLANKS_FAMILY, AtmosphericItemTags.GRIMWOOD_LOGS, GRIMWOOD_BOARDS.get(), GRIMWOOD_LADDER.get(), Atmospheric.MOD_ID);
+		WoodworksRecipeProvider.leafPileRecipes(consumer, GRIMWOOD_LEAVES.get(), GRIMWOOD_LEAF_PILE.get(), Atmospheric.MOD_ID);
 	}
 
 	public static ShapelessRecipeBuilder oneToOneConversionRecipe(RecipeCategory category, ItemLike output, ItemLike input, int count) {
